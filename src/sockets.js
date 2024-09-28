08534922d2b805f8320ab56243909c8102e7dc43
Leon Li <leontosy@gmail.com>
2024-09-28 03:00:24 -0700

    upgrade to node 18

--- src/sockets.js
+++ src/sockets.js
@@ -1,17 +1,28 @@
 import socketIO from 'socket.io';
 import uuid from 'node-uuid';
-import crypto from 'crypto';
+import { createHmac } from 'crypto';
 import redisAdapter from 'socket.io-redis';
-import { safeCb } from './util';
+import { safeCb } from './util.js';
 
 export default (server, config) => {
   const io = socketIO.listen(server);
-  io.adapter(redisAdapter({ host: config.redis.host, port: config.redis.port }));
+
+  const redisConfig = {
+    host: config.redis.host,
+    port: config.redis.port,
+  };
+  if (config.redis.password) {
+    redisConfig.auth_pass = config.redis.password;
+  }
+  if (config.redis.tls && Object.keys(config.redis.tls).length > 0) {
+    redisConfig.tls = { ...config.redis.tls };
+  }
+  io.adapter(redisAdapter(redisConfig));
   io.on('connection', (client) => {
     client.resources = {
       screen: false,
       video: true,
-      audio: false
+      audio: false,
     };
 
     // pass a message to another id
@@ -32,7 +43,7 @@ export default (server, config) => {
       if (client.room) {
         io.in(client.room).emit('remove', {
           id: client.id,
-          type
+          type,
         });
         if (!type) {
           client.leave(client.room);
@@ -46,16 +57,15 @@ export default (server, config) => {
       if (typeof name !== 'string') return;
       // check if maximum number of clients reached
       if (config.rooms && config.rooms.maxClients > 0) {
-        getClientCount(name)
-          .then((count) => {
-            if (count > config.rooms.maxClients) {
-              removeFeed();
-            }
-          });
+        getClientCount(name).then((count) => {
+          if (count > config.rooms.maxClients) {
+            removeFeed();
+          }
+        });
       }
       // leave any existing rooms
       removeFeed();
-      getClients(name, (err, clients) => safeCb(cb)(err, Object.assign({}, { you: client.id }, clients)));
+      getClients(name, (err, clients) => safeCb(cb)(err, { you: client.id, ...clients }));
       client.join(name);
       client.room = name;
     }
@@ -69,22 +79,21 @@ export default (server, config) => {
           });
           safeCb(callback)(null, obj);
         })
-        .catch(err => safeCb(callback)(err, null));
+        .catch((err) => safeCb(callback)(err, null));
     }
 
     function getClientCount(roomName, callback) {
-      clientsInRoom(roomName)
-        .then((num) => {
-          if (roomName) safeCb(callback)(num);
-        });
+      clientsInRoom(roomName).then((num) => {
+        if (roomName) safeCb(callback)(num);
+      });
     }
 
     function getClientId(callback) {
       safeCb(callback)(client.id);
     }
 
-    // we don't want to pass "leave" directly because the
-    // event type string of "socket end" gets passed too.
+    // we don't want to pass 'leave' directly because the
+    // event type string of 'socket end' gets passed too.
     client.on('disconnect', () => {
       removeFeed();
     });
@@ -93,12 +102,15 @@ export default (server, config) => {
       removeFeed();
     });
 
-    client.on('create', (name, cb) => {
-      if (arguments.length === 2) {
-        cb = (typeof cb === 'function') ? cb : () => {};
+    client.on('create', (...args) => {
+      let name; let
+        cb;
+      if (args.length === 2) {
+        [name, cb] = args;
+        cb = typeof cb === 'function' ? cb : () => {};
         name = name || uuid();
       } else {
-        cb = name;
+        [cb] = args;
         name = uuid();
       }
       // check if exists
@@ -121,25 +133,33 @@ export default (server, config) => {
     // tell client about stun and turn servers and generate nonces
     client.emit('stunservers', config.stunservers || []);
 
-    // create shared secret nonces for TURN authentication
-    // the process is described in draft-uberti-behave-turn-rest
-    const credentials = [];
-    // allow selectively vending turn credentials based on origin.
-    const { origin } = client.handshake.headers;
-    if (!config.turnorigins || config.turnorigins.includes(origin)) {
-      config.turnservers.forEach((server) => {
-        const hmac = crypto.createHmac('sha1', server.secret);
-        // default to 86400 seconds timeout unless specified
-        const username = `${Math.floor(new Date().getTime() / 1000) + (parseInt(server.expiry || 86400, 10))}`;
-        hmac.update(username);
-        credentials.push({
-          username,
-          credential: hmac.digest('base64'),
-          urls: server.urls || server.url
+    try {
+      // create shared secret nonces for TURN authentication
+      // the process is described in draft-uberti-behave-turn-rest
+      const credentials = [];
+      // allow selectively vending turn credentials based on origin.
+      const { origin } = client.handshake.headers;
+      if (!config.turnorigins || config.turnorigins.includes(origin)) {
+        config.turnservers.forEach((server) => {
+          const hmac = createHmac('sha1', server.secret);
+          // default to 86400 seconds timeout unless specified
+          const username = `${
+            Math.floor(new Date().getTime() / 1000)
+            + parseInt(server.expiry || 86400, 10)
+          }`;
+          hmac.update(username);
+          credentials.push({
+            username,
+            credential: hmac.digest('base64'),
+            urls: server.urls || server.url,
+          });
         });
-      });
+      }
+      client.emit('turnservers', credentials);
+    } catch (e) {
+      console.error('ERROR: ', e);
+      client.emit('turnservers', []);
     }
-    client.emit('turnservers', credentials);
   });
 
   function describeRoom(roomName) {
